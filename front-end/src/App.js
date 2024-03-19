a8442e1a5f128f2383302f6810fd4cc7ca8343b4
writersblockchain <seanmconrad94@gmail.com>
2024-03-19 13:24:40 -0400

    frontend

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -6,6 +6,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <input type="text" placeholder="amount of $SWOLE to send?" />
+        <input type="text" placeholder="wallet address to send $SWOLE to?" />
+        <button onClick={() => console.log("Hello, world!")}>Transfer</button>
       </header>
     </div>
   );
